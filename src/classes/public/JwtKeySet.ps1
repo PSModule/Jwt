6de5778b76aaf3390b7105fc16b4952b03f54096
Marius Storhaug <marstor@hotmail.com>
2026-05-12 22:45:57 +0200

    Add JWK Set (RFC 7517 §5) support and RFC 7638 thumbprint computation

---
+++ src/classes/public/JwtKeySet.ps1
@@ -0,0 +1,56 @@
+﻿class JwtKeySet {
+    [JwtKey[]] $keys = @()
+    [System.Collections.Specialized.OrderedDictionary] $AdditionalFields = [ordered]@{}
+
+    JwtKeySet() {}
+
+    JwtKeySet([JwtKey[]] $keys) {
+        if ($null -ne $keys) { $this.keys = $keys }
+    }
+
+    JwtKeySet([System.Collections.IDictionary] $values) {
+        if ($null -eq $values) { return }
+        foreach ($key in $values.Keys) {
+            if ($key -eq 'keys') {
+                $list = [System.Collections.Generic.List[JwtKey]]::new()
+                foreach ($entry in $values[$key]) {
+                    if ($entry -is [JwtKey]) { $list.Add($entry); continue }
+                    if ($entry -is [System.Collections.IDictionary]) {
+                        $list.Add([JwtKey]::new([hashtable]$entry))
+                        continue
+                    }
+                    throw [System.ArgumentException]::new(
+                        "JWK Set 'keys' entries must be JwtKey or IDictionary. Got [$($entry.GetType().FullName)]."
+                    )
+                }
+                $this.keys = $list.ToArray()
+            } else {
+                $this.AdditionalFields[$key] = $values[$key]
+            }
+        }
+    }
+
+    [JwtKey] FindByKid([string] $kid) {
+        foreach ($key in $this.keys) {
+            if ($key.kid -eq $kid) { return $key }
+        }
+        return $null
+    }
+
+    [System.Collections.Specialized.OrderedDictionary] ToOrderedDictionary() {
+        $o = [ordered]@{}
+        $keyDicts = [System.Collections.Generic.List[System.Collections.Specialized.OrderedDictionary]]::new()
+        foreach ($key in $this.keys) {
+            $keyDicts.Add($key.ToOrderedDictionary())
+        }
+        $o['keys'] = $keyDicts.ToArray()
+        foreach ($field in $this.AdditionalFields.Keys) {
+            $o[$field] = $this.AdditionalFields[$field]
+        }
+        return $o
+    }
+
+    [string] ToJson() {
+        return ConvertTo-Json -InputObject $this.ToOrderedDictionary() -Depth 100 -Compress
+    }
+}
---
+++ src/functions/public/Get-JwtKeyFromSet.ps1
@@ -0,0 +1,44 @@
+﻿function Get-JwtKeyFromSet {
+    <#
+        .SYNOPSIS
+        Looks up a [JwtKey] in a [JwtKeySet] by kid.
+
+        .DESCRIPTION
+        Returns the first [JwtKey] in the set whose kid matches. Returns $null when no
+        match is found. Pass -ErrorIfMissing to escalate to a non-terminating error.
+
+        .EXAMPLE
+        $key = Get-JwtKeyFromSet -KeySet $jwks -KeyId (Get-JwtHeader $token).kid
+
+        Resolves the signing key for a token by reading the header kid and looking it
+        up in a JWKS.
+
+        .OUTPUTS
+        JwtKey
+    #>
+    [OutputType([JwtKey])]
+    [CmdletBinding()]
+    param(
+        # The JWK Set to search.
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [ValidateNotNull()]
+        [JwtKeySet] $KeySet,
+
+        # The kid to find.
+        [Parameter(Mandatory, Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [string] $KeyId,
+
+        # Emit a non-terminating error when the kid is not found.
+        [Parameter()]
+        [switch] $ErrorIfMissing
+    )
+
+    process {
+        $match = $KeySet.FindByKid($KeyId)
+        if ($null -eq $match -and $ErrorIfMissing) {
+            Write-Error "No JWK in the set has kid='$KeyId'."
+        }
+        return $match
+    }
+}
